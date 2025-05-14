ad1737481795fcb4c3eff49a0068058dbab1a0b4
VimalaSellappan <Vimala.Sellappan@stud.sundsgarden.se>
2025-05-14 03:13:33 +0200

    React Assignment-2 completed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
-
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false); 
   return (
     <nav>
       <div className="left">
         <Link to="/">Home</Link>
       </div>
-      <div className="right">
+
+      <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        <div></div>
+        <div></div> 
+        <div></div>
+      </div>
+
+      <div className={`right ${menuOpen ? 'active' : ''}`}>
         <Link to="/about">About</Link>
         <Link to="/portfolio">Portfolio</Link>
       </div>
