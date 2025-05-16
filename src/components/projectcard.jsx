97c8ca59530241438a0d02466bb287fb7b469c4f
VimalaSellappan <Vimala.Sellappan@stud.sundsgarden.se>
2025-05-16 11:33:35 +0200

    Show limited project details on card before popup

--- src/components/projectcard.jsx
+++ src/components/projectcard.jsx
@@ -1,17 +1,21 @@
 // src/components/ProjectCard.jsx
-function ProjectCard({ name, screenshot, repo, tech, description, role, challenges, learning, onClick,}) {
+function ProjectCard({ name, screenshot, tech,onClick,}) {
     return (
       <div className="project-card" onClick={onClick}>
         <h2>{name}</h2>
         <img src={screenshot} alt={`${name} screenshot`} width="300" />
-        {repo && (
-          <p><strong>GitHub:</strong> <a href={repo} target="_blank" rel="noopener noreferrer">{repo}</a></p>
-        )}
         {tech && <p><strong>Tech used:</strong> {tech}</p>}
-      {description && <p><strong>Description:</strong> {description}</p>}
-      {learning && <p><strong>What I learned:</strong> {learning}</p>}
-      {role && <p><strong>My role:</strong> {role}</p>}
-      {challenges && <p><strong>Challenges solved:</strong> {challenges}</p>}
+        <button style={{
+          marginTop: '10px',
+          backgroundColor: 'blue',
+          color: 'white',
+          border: 'none',
+          padding: '8px 12px',
+          borderRadius: '5px',
+          cursor: 'pointer',
+          fontWeight: 'bold',
+          justifyContent:"center",
+          }}>Click me to view more</button>
       </div>
     );
   }
